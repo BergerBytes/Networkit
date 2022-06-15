0c5028e8d2d4496948f5e3fcbd9a0e671f9e0018
Michael Berger <michael@bergerbytes.io>
2022-06-15 07:56:22 -0500

    Add Result to URLSessionTask

--- Sources/Network/URLSessionNetworkTask/URLSessionNetworkTask.swift
+++ Sources/Network/URLSessionNetworkTask/URLSessionNetworkTask.swift
@@ -19,6 +19,7 @@ public class URLSessionNetworkTask<R: Requestable>: QueueableTask {
     public let delegate = MulticastDelegate<RequestDelegate>()
     public let requestIdentifier: RequestIdentifier?
     private let networkManager: NetworkManagerProvider
+    var result: Result<R, Error>?
     
     public required init(
         urlSession: URLSession = .shared,
@@ -157,10 +158,13 @@ public class URLSessionNetworkTask<R: Requestable>: QueueableTask {
             self.delegate.invokeDelegates { $0.requestCompleted(id: self.requestIdentifier) }
             self.dataCallbacks.forEach { $0(response) }
         }
+        
+        result = .success(response)
     }
     
     open func failed(error: Error) {
         delegate.invokeDelegates { $0.requestFailed(id: requestIdentifier, error: error) }
+        result = .failure(error)
     }
 }
 
