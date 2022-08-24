a0c86995802a6e1b971c80c16a6c63a209bebb71
Michael Berger <michael@bergerbytes.io>
2022-08-24 10:57:43 -0500

    🍒 Track token's canceled status
    # Conflicts:
    #	Sources/DeclarativeNetworking/Utility/CancellationToken.swift
    #	Sources/Network/Cacheable/Cacheable.swift

--- Sources/Network/Utility/CancellationToken.swift
+++ Sources/Network/Utility/CancellationToken.swift
@@ -3,13 +3,16 @@ import Cache
 public class CancellationToken {
     private let cancellationClosure: () -> Void
     let requestKey: String
-    
+
+    public private(set) var isCanceled = false
+
     public init(key: String, cancellationClosure: @escaping () -> Void) {
         self.requestKey = key
         self.cancellationClosure = cancellationClosure
     }
-    
+
     public func cancel() {
         cancellationClosure()
+        isCanceled = true
     }
 }
